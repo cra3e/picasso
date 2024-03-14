23578751276f1ead21e6b94e6d71bd491fcdb492
cra3e <cra3e@yandex.ru>
2024-03-15 00:02:58 +0300

    push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import {useInfiniteQuery} from 'react-query';
+import {useEffect} from 'react';
+import logo from './images/picasso_logo_horiz-0.png'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const fetchRepositories = async (page = 1) => {
+    const response = await fetch(
+        `https://jsonplaceholder.typicode.com/posts/?_per_page=30&_page=${page}`
+    );
+    return response.json();
+};
+
+const App = () => {
+    const {data, hasNextPage, fetchNextPage} = useInfiniteQuery(
+        "PICASSO_TEST_ASSIGNMENT",
+        ({pageParam = 1}) => fetchRepositories(pageParam),
+        {
+            getNextPageParam: (lastPage, allPages) => {
+                const maxPages = 10;
+                const nextPage = allPages.length + 1;
+                return nextPage <= maxPages ? nextPage : undefined;
+            },
+        }
+    );
+
+    useEffect(() => {
+        let fetching = false;
+        const onScroll = async (event) => {
+            const {scrollHeight, scrollTop, clientHeight} =
+                event.target.scrollingElement;
+
+            if (!fetching && scrollHeight - scrollTop <= clientHeight * 1.5) {
+                fetching = true;
+                if (hasNextPage) await fetchNextPage();
+                fetching = false;
+            }
+        };
+
+        document.addEventListener("scroll", onScroll);
+        return () => {
+            document.removeEventListener("scroll", onScroll);
+        };
+    }, []);
+
+    return (
+        <main>
+            <img src={logo} height="50px"/>
+            <br/>
+            <h1>ТЕСТОВОЕ ЗАДАНИЕ</h1>
+            <h4>Христиани Евгений</h4>
+            <ul>
+                {data.pages.map((page) =>
+                    page.map((e) => (
+                        <li key={e.id}>
+                            <p>
+                                <b>{e.id}. {e.title}</b>
+                            </p>
+                            <p>{e.body}</p>
+                        </li>
+                    ))
+                )}
+            </ul>
+        </main>
+    );
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { QueryClient, QueryClientProvider } from "react-query";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const client = new QueryClient({
+    defaultOptions: { queries: { suspense: true } },
+});
+
+ReactDOM.render(
+    <React.StrictMode>
+        <QueryClientProvider client={client}>
+            <React.Suspense fallback="Loading...">
+                <App />
+            </React.Suspense>
+        </QueryClientProvider>
+    </React.StrictMode>,
+    document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
